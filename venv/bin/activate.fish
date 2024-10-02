1feeb0a8f0f4c7c52880f3391db15a7d30d896c3
Amit Kundu <amit.kundu@dreambroker.fi>
2024-10-02 14:54:14 +0300

    venv file deleted

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/amitkundu/Intelleget cloud based studies/spot-hinta/venv"
+set -gx VIRTUAL_ENV "/Users/amit.kundu/spothinta/spot-hinta/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
